55499a1791ca4b125bfd9fd9af21de72f21a8635
Kuba <jakub.niedzwiedzki1990@gmail.com>
2018-10-02 21:50:12 +0200

    first commit

---
+++ content.js
@@ -0,0 +1,18 @@
+//variables checking today's date
+var dateToday = new Date();
+var today = dateToday.getDay()
+var todaysMonth = dateToday.getMonth()
+
+//variables checking tomorrow's date
+var dateTomorrow = new Date();
+dateTomorrow.setDate(dateToday.getDate()+1);
+var tomorrowsMonth = dateTomorrow.getMonth();
+
+//variable getting full date to find out if the reminder was shown already
+var fullDate = new Date().toLocaleDateString()
+
+chrome.storage.sync.get(['date'], function(result) {
+  if((today == 2 || todaysMonth != tomorrowsMonth) && result.date != fullDate){
+    chrome.runtime.sendMessage({todo: "showReminder"});
+  }
+});
